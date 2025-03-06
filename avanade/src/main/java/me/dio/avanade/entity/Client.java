6536b0372d32f8fda0aa2bcbd76c480ae7569f8f
Thamires <thamifernandesss123@gmail.com>
2025-03-06 14:27:03 -0300

    Feat: Id attribute and JPA annotations implemented

--- avanade/src/main/java/me/dio/avanade/entity/Client.java
+++ avanade/src/main/java/me/dio/avanade/entity/Client.java
@@ -1,17 +1,29 @@
 package me.dio.avanade.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
+@Entity(name="tb_client")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="name")
     private String name;
+
+    @Column(unique = true)
     private String cpf;
+
     private String phone;
 
 }
