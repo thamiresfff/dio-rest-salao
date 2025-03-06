e02a86132dd1a919391fd53a3d5a74b60da313af
Thamires <thamifernandesss123@gmail.com>
2025-03-05 23:08:14 -0300

    first commit

---
+++ avanade/src/main/java/me/dio/avanade/AvanadeApplication.java
@@ -0,0 +1,13 @@
+package me.dio.avanade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvanadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvanadeApplication.class, args);
+	}
+
+}
