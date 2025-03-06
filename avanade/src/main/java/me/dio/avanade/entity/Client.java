0547e62a90d55fb8cbae3661188e4b42fb066e45
Thamires <thamifernandesss123@gmail.com>
2025-03-06 13:38:57 -0300

    Feat: client created

---
+++ avanade/src/main/java/me/dio/avanade/entity/Client.java
@@ -0,0 +1,17 @@
+package me.dio.avanade.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Client {
+    private String name;
+    private String cpf;
+    private String phone;
+
+}
