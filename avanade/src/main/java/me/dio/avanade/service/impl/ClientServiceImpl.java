510d769c8dcbef531b8b01f8b4dd7dcc7a7121f6
Thamires <thamifernandesss123@gmail.com>
2025-03-07 15:33:25 -0300

    fix: dependency injection fixed

--- avanade/src/main/java/me/dio/avanade/service/impl/ClientServiceImpl.java
+++ avanade/src/main/java/me/dio/avanade/service/impl/ClientServiceImpl.java
@@ -1,17 +1,17 @@
 package me.dio.avanade.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import me.dio.avanade.entity.Client;
 import me.dio.avanade.repository.ClientRepository;
 import me.dio.avanade.service.ClientService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
+@RequiredArgsConstructor
 @Service
 public class ClientServiceImpl implements ClientService {
 
-    @Autowired
-    private ClientRepository clientRepository;
+
+    private final ClientRepository clientRepository;
 
     public Client SaveClient (Client client){
         return clientRepository.save(client);
--- avanade/src/main/java/me/dio/avanade/service/impl/EventServiceImpl.java
+++ avanade/src/main/java/me/dio/avanade/service/impl/EventServiceImpl.java
@@ -1,15 +1,16 @@
 package me.dio.avanade.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import me.dio.avanade.entity.Event;
 import me.dio.avanade.repository.EventRepository;
 import me.dio.avanade.service.EventService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class EventServiceImpl implements EventService {
-    @Autowired
-    private EventRepository eventRepository;
+
+    private final EventRepository eventRepository;
 
     public Event saveEvent (Event event){
         return eventRepository.save(event);
