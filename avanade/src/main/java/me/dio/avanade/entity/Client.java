bb21dc1e1bf6c15b4482ea80bc0175a126fa0e8a
Thamires <thamifernandesss123@gmail.com>
2025-03-06 21:33:03 -0300

    Feat: Id attribute and JPA annotations implemented

--- avanade/src/main/java/me/dio/avanade/entity/Client.java
+++ avanade/src/main/java/me/dio/avanade/entity/Client.java
@@ -6,24 +6,32 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 
 @Entity(name="tb_client")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+
 public class Client {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name="name")
+    @OneToMany(mappedBy = "cliente",cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Event> events;
+
+    @Column(name="name", nullable = false)
     private String name;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String cpf;
 
+    @Column(nullable = false, length = 11)
     private String phone;
+
 
 }
--- avanade/src/main/java/me/dio/avanade/entity/Event.java
+++ avanade/src/main/java/me/dio/avanade/entity/Event.java
@@ -1,22 +1,37 @@
 package me.dio.avanade.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import me.dio.avanade.enums.EventType;
 import me.dio.avanade.enums.PaymentStatus;
+import org.springframework.boot.autoconfigure.web.WebProperties;
 
 import java.time.LocalDateTime;
 
+@Entity(name="tb_event")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="client_id", nullable = false)
+    @ManyToOne
     private Client client;
+
+    @Column(name="date_time", unique = true)
     private LocalDateTime Date;
+
+    @Column(name="type_event",nullable = false)
     private EventType eventType;
+
+    @Column(name="status_pay",nullable = false)
     private PaymentStatus paymentStatus;
 
 }
