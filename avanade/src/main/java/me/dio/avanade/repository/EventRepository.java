8c0b195fe0799a0cf188668073b170b70f7b1fc6
Thamires <thamifernandesss123@gmail.com>
2025-03-11 14:12:27 -0300

    feat: method existsByDate implemented

--- avanade/src/main/java/me/dio/avanade/repository/EventRepository.java
+++ avanade/src/main/java/me/dio/avanade/repository/EventRepository.java
@@ -4,7 +4,10 @@ import me.dio.avanade.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+
 
 @Repository
 public interface EventRepository extends JpaRepository<Event,Long> {
+    boolean existsByDate(LocalDateTime data);
 }
